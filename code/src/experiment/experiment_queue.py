692235182d2e574b0875a63c4d4a07836019c049
unknown <nicolasdavidreinhart@googlemail.com>
2025-01-03 23:56:18 +0100

    Up logging quality for ExperimentQueue

--- code/src/experiment/experiment_queue.py
+++ code/src/experiment/experiment_queue.py
@@ -17,7 +17,7 @@ class ExperimentQueue:
     def run_all(self):
         self.logger.info("### RUNNING ALL EXPERIMENTS...")
 
-        # TODO: add error handling, track those that failed
+        self.successful_experiments = []
         self.failed_experiments = []
 
         while not self.queue.empty():
@@ -27,8 +27,10 @@ class ExperimentQueue:
             try:
                 experiment.run()
                 self.logger.info(f"### COMPLETED {experiment.name}.")
+                self.successful_experiments.append(experiment.name)
             except Exception as e:
-                self.logger.info(f"### ERROR in {experiment.name}: {str(e)}")
+                self.logger.error(f"### ERROR in {experiment.name}: {str(e)}")
+                self.failed_experiments.append(experiment.name)
             finally:
                 # Memory management
                 self.logger.info("### CLEARING SESSION AND COLLECTING GARBAGE...")
@@ -37,4 +39,11 @@ class ExperimentQueue:
 
             self.queue.task_done()
 
-        self.logger.info("### ALL EXPERIMENTS COMPLETED.")
+        if self.failed_experiments:
+            self.logger.warning(
+                f"### {len(self.failed_experiments)} EXPERIMENTS FAILED: {self.failed_experiments}"
+            )
+        else:
+            self.logger.info(
+                f"### ALL {len(self.successful_experiments)} EXPERIMENTS COMPLETED."
+            )
