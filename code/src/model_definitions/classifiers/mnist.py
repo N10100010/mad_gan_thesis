b6e7a8dfb18c9e8ab07c448b6c3b50defea415fd
N10100010 <nicolasdavidreinhart@googlemail.com>
2025-03-29 21:04:20 +0100

    LATEX: work on theory block

--- code/src/model_definitions/classifiers/mnist.py
+++ code/src/model_definitions/classifiers/mnist.py
@@ -12,11 +12,16 @@ class MNISTClassifier(BaseClassifier):
         # Functional Model Definition
         inputs = tf.keras.Input(shape=self.input_shape, name="classifier_input")
         x = tf.keras.layers.Conv2D(32, (3, 3), activation="relu")(inputs)
+        x = tf.keras.layers.BatchNormalization()(x)
         x = tf.keras.layers.MaxPooling2D((2, 2))(x)
+        
         x = tf.keras.layers.Conv2D(64, (3, 3), activation="relu")(x)
+        x = tf.keras.layers.BatchNormalization()(x)
         x = tf.keras.layers.MaxPooling2D((2, 2))(x)
+        
         x = tf.keras.layers.Flatten()(x)
         x = tf.keras.layers.Dense(128, activation="relu", name="feature_extractor")(x)
+        x = tf.keras.layers.Dropout(0.3)(x)
         outputs = tf.keras.layers.Dense(num_classes, activation="softmax")(x)
 
         # Functional Model
--- code/src/test.py
+++ code/src/test.py
@@ -55,9 +55,20 @@ def plot_scores(data):
 
 
 
-with open("/home/stud/n/nr063/mounted_home/mad_gan_thesis/code/experiments/CIFAR_MADGAN_MODELS_PROTOTYPES/2025-03-21_CIFAR_MADGAN_Experiment___big__latent_2048_3_gen_500_epochs/scores/metrics.json", "r") as f:
-    data = json.load(f)
+# with open("/home/stud/n/nr063/mounted_home/mad_gan_thesis/code/experiments/CIFAR_MADGAN_MODELS_PROTOTYPES/2025-03-21_CIFAR_MADGAN_Experiment___big__latent_2048_3_gen_500_epochs/scores/metrics.json", "r") as f:
+#    data = json.load(f)
+# plot_scores(data)
+
+from model_definitions.classifiers import CIFAR10Classifier
+import tensorflow as tf
+model = CIFAR10Classifier()
+
+_ = model(tf.random.normal(shape=(1, 32,32,3)))
+
+from tensorflow.keras.utils import plot_model
+
+import pydot_ng as pydot
+print(pydot.find_graphviz())
 
 
-
-plot_scores(data)
+# plot_model(model, to_file=".\model.png", show_shapes=True, show_layer_names=True)
